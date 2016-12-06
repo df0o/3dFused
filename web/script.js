a206ee35b51c4696e59e5690bb790b6d49e69d0d
df0o <mo.susie.yu@gmail.com>
2016-12-05 19:43:09 -0500

    fixed login/signup page. fixed form validation

--- web/script.js
+++ web/script.js
@@ -1,146 +1,162 @@
-function formValidation() { //main validation fuction
-	var ufirstn = document.registration.first_name; // variables from html files
-	var ulastn = document.registration.last_name;
-	var passid = document.registration.passid;
-	var uname = document.registration.username;
-  var idnumber = document.registration.idnumber;
-  var bday = document.registration.bday;
-	var uemail = document.registration.email;
-	var term = document.registration.terms;
-	var text; //text for displaying error message
+var strFields = "";
 
-	if (userfirstn_validation(ufirstn, 2, 12)) { // first name validation
-		if (userlastn_validation(ulastn, 2, 12)) { // last name validation
-			if (allLetter(uname)) { // user name validation
-        if (allNum(idnumber)) { // id validation
-          if (ValidateEmail(uemail)) { // email validation
-            if (passid_validation(passid, 7, 12)) { // password validation
-              if (checkTerms(term)) { // radio box validation
-                document.getElementById("errorMsg").innerHTML = " yay! you did it! "; //message if all correct
-              }
-            }
-          }
-				}
-			}
-		}
-	}
-	return false; // something went wrong...
-}
 
-function userfirstn_validation(ufirstn, mx, my) { // check name field is not empty and also within length
-	var ufirstn_len = ufirstn.value.length;
-	if (ufirstn_len == 0 || ufirstn_len >= my || ufirstn_len < mx) {
-		text = ("Please enter first name / length be between " + mx + " - " + my); // error messgage
-		document.getElementById("errorMsg").innerHTML = text; // show error message
-		return false;
-	}
-	return true;
-}
-
-function userlastn_validation(ulastn, mx, my) { // check name field is not empty and also within length
-	var ulastn_len = ulastn.value.length;
-	if (ulastn_len == 0 || ulastn_len >= my || ulastn_len < mx) {
-		text = ("Please enter last name / length be between " + mx + " - " + my);
-		document.getElementById("errorMsg").innerHTML = text;
-		return false;
-	}
-	return true;
-}
-
-function passid_validation(passid, mx, my) {  // password cant be empty
-	var passid_len = passid.value.length;
-	if (passid_len == 0 || passid_len >= my || passid_len < mx) { // also need to be within length
-		text = ("Please enter password / length be between " + mx + " - " + my);
-		document.getElementById("errorMsg").innerHTML = text;
-		return false;
-	}
-	return true;
-}
-
-function allLetter(uname) { // all leters for username
-	var letters = /^[A-Za-z]+$/;
-	if (uname.value.match(letters)) {
-		return true;
-	} else {
-		text = ("Username must have alphabet characters only");
-		document.getElementById("errorMsg").innerHTML = text;
-		return false;
-	}
-	return true;
-}
-
-function allNum(idnumber) { // all numbers 
-	if (idnumber.value.length == 0 || !Number.isNAN(idnumber)) { // cant be empty
-		text = ("ID cannot be empty");
-		document.getElementById("errorMsg").innerHTML = text;
-		return false;
-	}
+function signupValidation() {
+  var missingFields = false;
+  var div = document.getElementById("errorbox");
   
-  key = String.fromCharCode(idnumber);
-	
+  if (!validateUsername()){
+    missingFields = true;
+  }
+  if (!validateName()) {
+    missingFields = true;
+  }
+  if (!validatePassword()) {
+    missingFields = true;
+  }
+  if (!validateEmail()) {
+    missingFields = true;
+  }
+  if (!validateAge()) {
+    missingFields = true;
+  }
+  if (missingFields) {
+    div.style.display = "block";
+    document.getElementById("errorMsg").innerHTML = ("I'm sorry, but you must correct the following error(s) before continuing:<br><br>" + strFields );
+    strFields = "";
+		return false;
+  }
   return true;
 }
 
-function ValidateEmail(uemail) { // email validation
-	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	if (uemail.value.match(mailformat)) {
-		return true;
-	} else {
-		text = ("You have entered an invalid email address!");
-		document.getElementById("errorMsg").innerHTML = text;
+function loginValidation() {
+  var missingFields = false;
+  var div = document.getElementById("errorbox");
+  
+  if (!validateUsername()){
+    missingFields = true;
+  }
+  if (!validatePassword()) {
+    missingFields = true;
+  }
+  if (missingFields) {
+    div.style.display = "block";
+    document.getElementById("errorMsg").innerHTML = ("I'm sorry, but you must correct the following error(s) before continuing:<br><br>" + strFields );
+    strFields = "";
 		return false;
-	}
-	return true;
+  }
+  return true;
 }
 
-function checkTerms(term) { //need to select terms
-	if (term.checked == false) {
-		text = ("You must agree to the terms first.");
-		document.getElementById("errorMsg").innerHTML = text;
-		return false;
-	} else {
-		return true;
-	}
+function validateUsername() {
+    // Get value entered into Username text field
+    var checkVal = document.registration.username.value;
+
+    // User Name not empty
+    if (checkVal.length == 0) {
+      strFields += "- Username cannot be empty!<br>";
+      return false;
+    }
+
+    // User Name alphanumeric (case-insensitive)
+    if (!(/^[a-z0-9]+$/i.test(checkVal))) {
+        strFields += "- Username must be alphanumeric!<br>";
+        return false;
+    }
+
+    // User Name length: 6-20 (inclusive)
+    if (checkVal.length < 6 || checkVal.length > 20) {
+        strFields += "- Username must be at least 6 and 20 characters long characters long!<br>";
+        return false;
+    }
+    return true;
 }
 
-function initMap() { // google map api
-  var locations = [ // multiple locations/markers
-      ['content', 43.2605738, -79.9304626, 4], //content can be changed to display different things
-      ['content', 43.2805738, -79.9504626, 5],
-      ['content', 43.2005738, -79.9004626, 3],
-      ['content', 43.3005738, -79.9204626, 2],
-      ['content', 43.2505738, -79.8804626, 1]
-    ];
-  
-  var mapOptions = { // map options 
-    zoom: 11,
-    center: new google.maps.LatLng(43.2605738, -79.9304626),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  
-  var infowindow = new google.maps.InfoWindow(); // info window for markers
-  var marker, i;
-  
-  var contentString = '<div id="content">'+ // content in info window
-    '<h1 id="firstHeading" class="firstHeading">MakerBot Replicator Desktop 3D Printer</h1>'+
-    '<p>The replicator desktop model provides a large build volume and fast print time to accelerate rapid prototyping and model making.</p>'+
-    '<p>MakerBot: , <a href="./individual_sample.html">'+
-    'Click here to go to posting</a> '+
-    '</div>';
-  
-  for (i = 0; i < locations.length; i++) { // display all markers
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map: map
-    });
+function validateName() {
+    var checkVal = document.registration.name.value;
 
-    google.maps.event.addListener(marker, 'click', (function(marker, i) { // show info window when click marker
-      return function() {
-        infowindow.setContent(contentString); //locations[i][0]
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
+    // First name not empty
+    if (checkVal.length == 0) {
+        strFields += "- Name cannot be empty!<br>";
+        return false;
+    }
+
+    // First Name under 30 characters
+    if (checkVal.length > 30) {
+        strFields += "- Name cannot be over 30 characters long!<br>";
+        return false;
+    }
+
+    // First Name letters only (case-insensitive)
+    if (!(/^[a-z]+$/i.test(checkVal))) {
+        strFields += "- Name Must be alphabetic!<br>";
+        return false;
+    }
+    return true;
+}
+
+// Validate Password field. Return true if it passes all tests, else false
+function validatePassword() {
+    var checkVal = document.registration.passid.value;
+
+    // Password not empty
+    if (checkVal.length == 0) {
+        strFields += "- Password cannot be empty!<br>";
+        return false;
+    }
+
+    // Password length: 6-20 (inclusive)
+    if (checkVal.length < 6 || checkVal.length > 20) {
+        strFields += "- Password must be between 6 and 20 characters long!<br>";
+        return false;
+    }
+
+    return true;
+}
+
+function validateEmail() {
+    var checkVal = document.registration.email.value;
+
+    // Email not empty
+    if (checkVal.length == 0) {
+        strFields += "- Email cannot be empty!<br>";
+        return false;
+    }
+
+    if (!(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(checkVal))) {
+        strFields += "- Invalid Email format!<br>";
+        return false
+    }
+
+    return true;
+}
+
+function validateAge() {
+    var checkVal = document.registration.age.value;
+
+    // Age not empty
+    if (checkVal.length == 0) {
+        strFields += "- Age cannot be empty!<br>";
+        return false;
+    }
+
+    // Age is 10-120
+    if (checkVal.length < 10 || checkVal.length > 120) {
+        strFields += "- Invalid age!<br>";
+        return false;
+    }
+
+    // Age is numeric characters only
+    if (!(/^[0-9]+$/.test(checkVal))) {
+        strFields += "- Age must be numeric!<br>";
+        return false;
+    }
+
+    return true;
+}
+
+
+function hideError() {
+  var div = document.getElementById("errorbox");
+  div.style.display = "none";
 }
